74f2e037c490e8cb471b304ea62279ad6d0a89bd
Soumyadip Moni <116944847+AvaterClasher@users.noreply.github.com>
2024-01-17 01:26:22 +0530

    Rewrote code and fixed bugs

--- examples/bug.py
+++ examples/bug.py
@@ -4,9 +4,9 @@ a = random.randint(1, 12)
 b = random.randint(1, 12)
 
 for i in range(10):
-    question = "What is " + a + " x " + b + "? "
+    question = "What is " + str(a) + " x " + str(b) + "? "
     answer = int(input(question))
-    if answer = a * b:
+    if answer == a * b:
         print("Well done!")
     else:
         print("No.")
